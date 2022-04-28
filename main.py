ff8bd4a48b9a0efaf220759542f093a1e6c1961e
yuregl <yuregaldino@hotmail.com>
2022-04-27 22:01:01 -0300

    feat: add funçao para ler arquivo

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import conversion
 import filters
+import utils as ut
 
 if __name__ == "__main__":
   file = "./assets/Woman.png"
+  file_txt  = './assets/input3.txt' 
   while(True): 
     selectInput = input(
         "\t+---+-------------------------------------------------------+\n"
@@ -10,6 +12,7 @@ if __name__ == "__main__":
         +"\t+---+-------------------------------------------------------+\n"
         +"\t| 1 | RGB-YIQ-RGB                                           |\n"
         +"\t| 2 | Negativo                                              |\n"
+        +"\t| 3 | Correlação                                            |\n"
         +"\t| 4 | Mediana na banda Y                                    |\n"
         +"\t| 0 | Sair                                                  |\n"
         +"\t+---+-------------------------------------------------------+\n")
@@ -21,6 +24,10 @@ if __name__ == "__main__":
       negative = conversion.negative(file, 'NEGATIVO')
       y_matrix, i_matrix, q_matrix = conversion.convert_to_yiq(file)
       conversion.negative_in_y(y_matrix, i_matrix, q_matrix,'NEGATIVO_IN_Y')
+
+    if(selectInput == '3'):
+      values = ut.read_file(file_txt)
+      print(values)
 
     if(selectInput == '4'):
       row = int(input("Tamanaho das linhas: "))
--- utils.py
+++ utils.py
@@ -35,3 +35,38 @@ def create_extended_matrix_y(y_matrix, i_matrix, q_matrix, inc_row ,inc_col):
 			q_extended[i + inc_row, j + inc_col] = q_matrix[i, j]
 
 	return y_extended, i_extended, q_extended
+
+def read_file(path):
+	all_file = open(path, "r")
+	read_lines = all_file.readlines()
+	offset=int(read_lines[0].split()[1])
+	row=int(read_lines[1].split()[1])
+	col=int(read_lines[2].split()[1])
+	line_pivo = 5 + 2*row
+	pivo_row = int(read_lines[line_pivo].split()[1])
+	pivo_col = int(read_lines[line_pivo].split()[2])
+
+	mask_sobel_horizontal = []
+	mask_sobel_vertical = []
+
+	for row_mask in range(4, row+4):
+		aux_arr = []
+		for col_mask in range(col):
+			arr_line = read_lines[row_mask].split()
+			aux_arr.append(int(arr_line[col_mask]))
+		mask_sobel_horizontal.append(aux_arr)
+
+	begin_line_mask_vertical = 5 + row
+	end_line_mask_vertical = begin_line_mask_vertical+row
+
+	for row_mask in range(begin_line_mask_vertical, end_line_mask_vertical):
+		aux_arr = []
+		for col_mask in range(col):
+			arr_line = read_lines[row_mask].split()
+			aux_arr.append(int(arr_line[col_mask]))
+		mask_sobel_vertical.append(aux_arr)
+	
+
+	return offset, row, col, mask_sobel_horizontal, mask_sobel_vertical, pivo_row, pivo_col
+
+
